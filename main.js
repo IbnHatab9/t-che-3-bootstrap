79344ee635bc3bcf661d203ad43634e2dba2f169
IbnHatab9 <faye12092@gmail.com>
2024-07-25 09:56:48 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let nav = documenet.querySelector("navbar");
+let nav = document.querySelector("navbar");
 window.onscroll = function () {
     if(document.documentElement.scrollTop > 20){
         nav.classList.add("header-scrolled");
